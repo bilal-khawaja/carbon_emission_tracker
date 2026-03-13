9ca7bba1aafbf36e14da711aa341bb85a4cbea6a
Bilal <khawajab302@gmail.com>
2026-03-13 18:59:38 +0100

    changes: env var applied in database.py

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,11 @@
 from typing import Any, Generator
 from sqlmodel import create_engine, Session
-
+from dotenv import load_dotenv
 import os
 
-database_url = f"sqlite:///table1.db"
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")    
  
 engine = create_engine(
     database_url,
